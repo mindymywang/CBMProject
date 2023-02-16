265241e602618474f000798c0b74c98118aac331
mindymywang <mindy.wang@gmail.com>
2023-02-16 14:03:35 +0800

    提交DB設定

--- CBM/map/models.py
+++ CBM/map/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Business_Domain(models.Model):
+    bd_no = models.CharField(primary_key=True, max_length=20),
+    bd_name = models.CharField(max_length=50, null=True),
+    bd_eng_name = models.CharField(max_length=50)
+
+
